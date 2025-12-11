4dabb7af6682b4003d547e25936e361591561a05
= <=>
2025-12-11 06:09:40 -0500

    added precommit

--- target/release/deps/_core.d
+++ target/release/deps/_core.d
@@ -2,8 +2,6 @@
 
 /Users/andy/rep-grow/target/release/deps/lib_core.dylib: src/lib.rs src/stockfish/mod.rs src/study/mod.rs
 
-/Users/andy/rep-grow/target/release/deps/lib_core.rlib: src/lib.rs src/stockfish/mod.rs src/study/mod.rs
-
 src/lib.rs:
 src/stockfish/mod.rs:
 src/study/mod.rs:
